20ccdf0f95ea9b6ab7432d713893854ace7b1f32
Toby Smith <tobysmith568@hotmail.co.uk>
2020-06-14 15:57:36 +0100

    Added functionality to run the command from the command palette

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-# run-in-powershell README
+# Run-in-Powershell README
 
-This is the README for your extension "run-in-powershell". After writing up a brief description, we recommend including the following sections.
+This extention allows you to run `.ps1` files in a powershell terminal **external** to VS Code.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
 
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
 
 ## Requirements
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,37 @@
 import * as vscode from "vscode";
-import { run } from "./run";
+import * as util from "util";
+
+const exec = util.promisify(require("child_process").exec);
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand("run-in-powershell.runInPS", async (d: { fsPath: string }) => await run(d.fsPath));
+	let disposable = vscode.commands.registerCommand("run-in-powershell.runInPS", async (d: { fsPath: string }) => {
+
+		let location: string | undefined= d?.fsPath;
+
+		if (location === undefined) {
+			location = vscode.window.activeTextEditor?.document.fileName;
+		}
+
+		if (location === undefined) {
+			vscode.window.showErrorMessage("No file is currently open");
+			return;
+		}
+
+		await run(location);
+	});
 	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
+
+export async function run(location: string): Promise<void> {
+	const command: string = `START powershell -noexit "& ""${location}"""`;
+	console.log("Running Powershell command:", command);
+
+	try {
+		await exec(command);
+	} catch (err) {
+		console.error(err);
+		vscode.window.showErrorMessage(JSON.stringify(err));
+	};
+}
