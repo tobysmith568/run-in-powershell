8e4623366f907340157bf6476b6816b31cd3de18
Toby Smith <tobysmith568@hotmail.co.uk>
2020-06-14 18:59:02 +0100

    Added an admin command

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,53 @@
 import * as vscode from "vscode";
 import * as util from "util";
+import { dirname } from "path";
 
 const exec = util.promisify(require("child_process").exec);
+const adminSection: string = "-Verb runAs";
+
+type ContextData = {
+	fsPath: string
+};
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand("run-in-powershell.runInPS", async (d: { fsPath: string }) => {
+	let runInPSDisposable = vscode.commands.registerCommand("run-in-powershell.runInPS",
+						async (data: ContextData) => runInPowershell(data, false));
 
-		let location: string | undefined= d?.fsPath;
+	let runInPSAdminDisposable = vscode.commands.registerCommand("run-in-powershell.runInPSAdmin",
+						async (data: ContextData) => runInPowershell(data, true));
 
-		if (location === undefined) {
-			location = vscode.window.activeTextEditor?.document.fileName;
-		}
-
-		if (location === undefined) {
-			vscode.window.showErrorMessage("No file is currently open");
-			return;
-		}
-
-		await run(location);
-	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(runInPSDisposable);
+	context.subscriptions.push(runInPSAdminDisposable);
 }
 
 export function deactivate() {}
 
-export async function run(location: string): Promise<void> {
-	const command: string = `START powershell -noexit "& ""${location}"""`;
+export async function runInPowershell(contextData: ContextData, asAdmin: boolean): Promise<void> {
+	const fileLocation: string | undefined = getFileLocation(contextData);
+
+	if (fileLocation === undefined) {
+		vscode.window.showErrorMessage("No file is currently open");
+		return;
+	}
+
+	await run(fileLocation, asAdmin);
+}
+
+export function getFileLocation(contextData?: ContextData): string | undefined {
+	let location: string | undefined = contextData?.fsPath;
+
+	if (location === undefined) {
+		location = vscode.window.activeTextEditor?.document.fileName;
+	}
+
+	return location;
+}
+
+export async function run(location: string, admin: boolean = false): Promise<void> {
+	const workingDir: string = dirname(location);
+
+	const command: string = `PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -NoExit -ExecutionPolicy Unrestricted -Command cd ${escapeSpaces(workingDir)} ; & ${escapeSpaces(location)}""' ${admin ? adminSection : ""}}"`;
+
 	console.log("Running Powershell command:", command);
 
 	try {
@@ -34,4 +56,8 @@ export async function run(location: string): Promise<void> {
 		console.error(err);
 		vscode.window.showErrorMessage(JSON.stringify(err));
 	};
+}
+
+export function escapeSpaces(value: string): string {
+	return value.replace(" ", "` ");
 }
